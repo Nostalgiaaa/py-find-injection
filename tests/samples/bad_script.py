68d64ba619328a50d033d01fcaa71d985d6213fa
fanbowen <844255371@qq.com>
2019-03-05 18:33:04 +0800

    补充一下单元测试

--- tests/samples/bad_script.py
+++ tests/samples/bad_script.py
@@ -8,3 +8,7 @@ DAO.raw_sql("SELECT foo from bar WHERE id=%s" % 2)
 DAO.raw_sql("SELECT foo FROM bar where id={0}".format(1))
 
 DAO.raw_sql("SELECT foo FROM bar where id={id}".format(id=1))
+
+DAO.raw_sql("SELECT foo FROM {0} where id={1}".format("aa", 1))
+
+DAO.raw_sql("SELECT foo FROM {table} where id={id}".format(table="aa", id=1))
